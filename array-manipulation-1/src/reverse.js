91c9b4c85df94c749d5cf0731752ef790359315b
Jiheng Nie <Niejiheng@gmail.com>
2022-09-02 17:41:36 +0000

    Used reverse order for the loop

--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -9,8 +9,9 @@ return result
 */
 function reverse(array) {
   var result = [];
-  for (var i = 0; i < array.length; i++) {
-    result.push(array[array.length - 1 - i]);
+
+  for (var i = array.length - 1; i >= 0; i--) {
+    result.push(array[i]);
   }
   return result;
 }
