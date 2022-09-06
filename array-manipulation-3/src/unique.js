dc9f5dd2a0ad2740bb12b2408c986183a514effb
Jiheng Nie <Niejiheng@gmail.com>
2022-09-06 23:56:21 +0000

    In progress

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,15 @@
 /* exported unique */
+/*
+need an output array
+need a for loop
+  for each index, check if the item is in the output array
+  if no, add to result array
+return output array
+*/
+
+function unique(array) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    result.push(1);
+  }
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,30 @@
 /* exported zip */
+/*
+need an output array
+need a temp array
+need a variable for length
+  set length to the length of first array
+  if length of second is shorter, set the length to the second length
+use a for loop
+  push first[i] and second[i] into temp array
+  push temp array into output array
+  reset temp array
+  stop when length is hit
+return output array
+*/
+
+function zip(first, second) {
+  var result = [];
+  var temp = [];
+  var length = first.length;
+  if (length > second.length) {
+    length = second.length;
+  }
+  for (var i = 0; i < length; i++) {
+    temp.push(first[i]);
+    temp.push(second[i]);
+    result.push(temp);
+    temp = [];
+  }
+  return result;
+}
