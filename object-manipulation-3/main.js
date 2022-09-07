474b9f5cb6bdd53d238e5636f366cb5b02a0bbbb
Jiheng Nie <Niejiheng@gmail.com>
2022-09-07 21:11:35 +0000

    Starting

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,28 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+/*
+Need a players object with a name string and a cards array
+Need
+
+var players = [
+  {
+    name: 'Default 1',
+    cards: []
+  },
+  {
+    name: 'Default 2',
+    cards: []
+  }
+];
+
+var deck = {
+  rank: ['Ace', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'],
+  suit: ['Clubs', 'Diamonds', 'Hearts', 'Spacdes']
+};
+
+// var item = items[Math.floor(Math.random() * items.length)]; // Selecting random items
+
+function convertCardsIntoPoints(cardObject) {
+
+}
+*/
