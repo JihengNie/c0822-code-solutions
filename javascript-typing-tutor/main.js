3dffc650cb157d181317c27306acf15c0e231188
Jiheng Nie <Niejiheng@gmail.com>
2022-09-08 23:56:10 +0000

    In progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,5 +1,5 @@
 var $letterNodeList = document.querySelectorAll('.typed-no');
-var $playAgain = document.querySelectorAll('.play-again');
+var $playAgain = document.querySelector('.play-again');
 
 window.addEventListener('keydown', typingLetters, true);
 var i = 0;
@@ -9,11 +9,12 @@ function typingLetters(event) {
   }
   if (event.key === $letterNodeList[i].textContent && ($letterNodeList[i].className === 'typed-no current' || $letterNodeList[i].className === 'typed-wrong current')) {
     $letterNodeList[i].className = 'typed-correct';
-    $letterNodeList[i + 1].className = 'typed-no current';
+    if (i < $letterNodeList.length) {
+      $letterNodeList[i + 1].className = 'typed-no current';
+    }
     i++;
   }
-  if (i - 1 === $letterNodeList.length) {
-    $playAgain.className = 'play-again';
-  }
-
+}
+if (i - 1 === $letterNodeList.length) {
+  $playAgain.className = 'play-again';
 }
