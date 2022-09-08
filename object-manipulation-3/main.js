485197225cf792de8eeba0f923976b0f0e1af842
Jiheng Nie <Niejiheng@gmail.com>
2022-09-08 20:32:05 +0000

    In progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -10,7 +10,9 @@ Need an array for
 
 need a function for assigning 2 cards to each player
 */
+
 /*
+
 var playerName = ['Player1', 'Player2', 'Player3', 'Player4'];
 var PlayerHand = [];
 var cardRank = ['Ace', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
@@ -32,15 +34,27 @@ var newDeck = creatingNewDeck(cardRank, cardSuit);
 function shuffleDeck(deck) {
   var shuffledDeck = [];
   var totalCards = 52;
-  var randomIndex = Math.floor(Math.random() * totalCards);
   for (var k = 0; k < deck.length; k++) {
+    var randomIndex = Math.floor(Math.random() * (totalCards - k));
     if (!_.includes(shuffledDeck, deck[randomIndex])) {
       shuffledDeck.push(deck[randomIndex]);
-      totalCards--;
     }
   }
   return shuffledDeck;
 }
 
-console.log(shuffleDeck(newDeck));
+function removeItemFromArrayByIndex(index, array) {
+  var firstHalf = array.slice(0, index);
+  var secondHalf = array.slice(index + 1);
+  if (index === 0) {
+    return array.slice(index + 1);
+  }
+  var result = _.union(firstHalf, secondHalf);
+  return result;
+}
+
+function removeItemFromArray(item, array) {
+  var indexOfItem = array.indexOf(array[item]);
+  return removeItemFromArrayByIndex(indexOfItem, array);
+}
 */
