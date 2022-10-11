862254ee26d61527d7c0012967a6c576abd0eded
Jiheng Nie <Niejiheng@gmail.com>
2022-10-11 23:55:05 +0000

    Updated variable names

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -2,19 +2,19 @@ const num1 = parseInt(process.argv[2], 10);
 const operation = process.argv[3].toLowerCase();
 const num2 = parseInt(process.argv[4], 10);
 
-const add = require('./add');
-const subtract = require('./subtract');
-const multiply = require('./multiply');
-const divide = require('./divide');
+const addFile = require('./add');
+const subtractFile = require('./subtract');
+const multiplyFile = require('./multiply');
+const divideFile = require('./divide');
 
 if (operation === 'add' || operation === 'plus') {
-  console.log(`result: ${add.add(num1, num2)}`);
+  console.log(`result: ${addFile.add(num1, num2)}`);
 } else if (operation === 'subtract' || operation === 'minus') {
-  console.log(`result: ${subtract.subtract(num1, num2)}`);
+  console.log(`result: ${subtractFile.subtract(num1, num2)}`);
 } else if (operation === 'multiply' || operation === 'times') {
-  console.log(`result: ${multiply.multiply(num1, num2)}`);
+  console.log(`result: ${multiplyFile.multiply(num1, num2)}`);
 } else if (operation === 'divide' || operation === 'over') {
-  console.log(`result: ${divide.divide(num1, num2)}`);
+  console.log(`result: ${divideFile.divide(num1, num2)}`);
 } else {
   console.log('Invalid Expression');
 }
