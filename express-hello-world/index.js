dbc6d600f6febd88b36b78c1bb162613911e3201
Jiheng Nie <Niejiheng@gmail.com>
2022-10-13 23:57:29 +0000

    Forgot to add a listening message

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -5,4 +5,7 @@ app.use((req, res) => {
   res.send('A string that I am sending');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
