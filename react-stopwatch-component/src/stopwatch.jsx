7e849cf584e7acd5aa0e877defdeae8b9b04c248
Jiheng Nie <Niejiheng@gmail.com>
2022-10-25 21:35:48 +0000

    Combined play and paused into one boolean

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,36 +4,28 @@ export default class StopWatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      play: false,
-      pause: true,
+      isStarted: false,
       time: 0
     };
-    this.handlePlay = this.handlePlay.bind(this);
-    this.handlePause = this.handlePause.bind(this);
+    this.handleStarted = this.handleStarted.bind(this);
     this.handleReset = this.handleReset.bind(this);
   }
 
-  handlePlay() {
-    if (!this.state.play) {
+  handleStarted() {
+    if (!this.state.isStarted) {
       this.interval = setInterval(() => {
         this.setState(({ time }) => ({ time: time + 1 }));
       }, 1000);
-      this.setState({ play: true });
-      this.setState({ pause: false });
-    }
-  }
-
-  handlePause() {
-    if (!this.state.pause) {
-      this.setState({ play: false });
-      this.setState({ pause: true });
+      this.setState({ isStarted: true });
+    } else {
+      this.setState({ isStarted: false });
       clearInterval(this.interval);
     }
   }
 
   handleReset() {
-    if (this.state.pause) {
-      this.setState({ time: 0, play: false, pause: true });
+    if (!this.state.isStarted) {
+      this.setState({ time: 0, isStarted: false });
     }
   }
 
@@ -42,9 +34,9 @@ export default class StopWatch extends React.Component {
       <div className='container'>
         <button onClick={this.handleReset}> {this.state.time} </button>
         <span>
-          {!this.state.play
-            ? <i className="fa-solid fa-play fa-3x" onClick={this.handlePlay}></i>
-            : <i className="fa-solid fa-pause fa-3x" onClick={this.handlePause}></i>
+          {!this.state.isStarted
+            ? <i className="fa-solid fa-play fa-3x" onClick={this.handleStarted}></i>
+            : <i className="fa-solid fa-pause fa-3x" onClick={this.handleStarted}></i>
             }
         </span>
       </div>
