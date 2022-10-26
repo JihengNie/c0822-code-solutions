59f16d99db3dabcd78a2c0c70fdcb86eadde6d26
Jiheng Nie <Niejiheng@gmail.com>
2022-10-26 23:58:24 +0000

    Adjusted languages to be a prop and updated clickId to use null

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,29 +4,27 @@ export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clickId: '000',
-      shown: false
+      clickId: null
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
-    this.setState({ shown: !this.state.shown });
-    this.setState({ clickId: event.target.parentNode.id });
+    if (event.target.parentNode.id === this.state.clickId) {
+      this.setState({ clickId: null });
+    } else {
+      this.setState({ clickId: event.target.parentNode.id });
+    }
   }
 
   render() {
-    const languages = [
-      { langId: '001', name: 'HML', description: 'Words about HML' },
-      { langId: '002', name: 'CSS', description: 'Words about CSS' },
-      { langId: '003', name: 'JS', description: 'Words about JS' }
-    ];
+    const languages = this.props.data;
     const languagesList = <div className='accordion'>{
       languages.map(items => {
         return (
           <div className='container' id={items.langId} key={items.langId}>
             <div onClick={this.handleClick} className='label'>{items.name}</div>
-            <div className={ this.state.clickId === items.langId && this.state.shown ? 'content' : 'hidden' }>
+            <div className={ this.state.clickId === items.langId ? 'content' : 'hidden' }>
               {items.description}
             </div>
           </div>
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -5,4 +5,10 @@ import Accordion from './accordion';
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<Accordion/>);
+const languages = [
+  { langId: '001', name: 'HML', description: 'Words about HML' },
+  { langId: '002', name: 'CSS', description: 'Words about CSS' },
+  { langId: '003', name: 'JS', description: 'Words about JS' }
+];
+
+root.render(<Accordion data={languages}/>);
