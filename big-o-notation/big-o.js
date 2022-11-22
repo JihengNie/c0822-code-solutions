4cf271de37cf4acf47269e933144dec996f541f2
Jiheng Nie <Niejiheng@gmail.com>
2022-11-22 00:05:40 +0000

    Fixed comparing === word simple steps number

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -32,7 +32,7 @@ function uniqueQuadratic(words) {
       c++                           // 2 * n * n = O(n^2)
     ) {
       const comparing = words[c];   // 2 * n * n = O(n^2)
-      if (comparing === word) {     // 1 * n * n = O(n^2)
+      if (comparing === word) {     // 2 * n * n = O(n^2)
         isUnique = false;           // 1 * n * n = O(n^2)
       }
     }
